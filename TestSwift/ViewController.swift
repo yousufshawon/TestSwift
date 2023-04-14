df5eb8589696d2342321d2bdab0715af84bc2880
Yousuf <yousuf.kuet08@gmail.com>
2023-04-14 10:26:48 +0600

    Complete assignment

--- TestSwift/ViewController.swift
+++ TestSwift/ViewController.swift
@@ -13,9 +13,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-    
+        // odd check
+        for num in 1...5 {
+            oddChecker(num:num, checker: isOdd(number:))
+        }
+
+        // frequency check
+        showCharFrequency(of: "Anonymous")
+        
+        //add suffix
+        var addSuffixClosure = getAddSuffixClouser(sufix: "ly")
+        print(addSuffixClosure("hopeless"))
+        print(addSuffixClosure("total"))
+        
+        addSuffixClosure = getAddSuffixClouser(sufix: "less")
+        print(addSuffixClosure("fear"))
+        print(addSuffixClosure("ruth"))
+        
     }
     
+    
+    func oddChecker( num:Int, checker : (Int) -> Bool) {
+        print("\(num) is odd: \(checker(num))")
+    }
+    
+    func isOdd(number:Int) -> Bool {
+        return number % 2 == 1;
+    }
+    
+    func showCharFrequency(of str: String) {
+        var freqDictionary : [Character: Int] = [:]
+        for ch in str {
+            let freq = (freqDictionary[ch] ?? 0) + 1
+            //print("[\(ch)] = \(freq)")
+            freqDictionary[ch] = freq
+        }
+        
+        for entry in freqDictionary {
+            print("\(entry.key) = \(entry.value)")
+        }
+    }
+    
+    func getAddSuffixClouser (sufix : String) -> (String) -> String {
+        return {
+            (str : String) -> String in
+            return str + sufix
+        }
+    }
 
 
 }
